6e2caaefa6fdf6c3aba020b04a0a04a166dd950a
AleFigueiredoo <alessanderfs2k@gmail.com>
2022-06-26 18:31:48 -0300

    Refatorando código

--- Segunda_lei_ohm.py
+++ Segunda_lei_ohm.py
@@ -1,48 +1,64 @@
 def calculo_corrente():
     r = float(input("Digite o valor da resistência em Ohms: "))
     v = float(input("Digite o valor da tensão: "))
-    print("O valor da corrente é", round(v / r,3), "A")
-    return
+    return round(v / r,3)
 
 def calculo_tensao():
     r = float(input("Digite o valor da resistência em Ohms: "))
     i = float(input("Digite o valor da Corrente: "))
-    print("O valor da tensão é", round(r * i), "V")
-    return
+    return round(r * i)
 
 def calculo_resistencia():
     v = float(input("Digite o valor da tensão: "))
     i = float(input("Digite o valor da Corrente: "))
-    print("O valor da resitência é ", round(v / i,3), "ohms")
-    return
+
+    return round(v / i,3)
 
 def calculo_potencia():
     v = float(input("Digite o valor da tensão: "))
     i = float(input("Digite o valor da Corrente: "))
-    print("O valor da potência é ", round(v * i,3), "W")
-    return
 
-letra = input("Digite:\n\"R\" para resistência   "
+    return round(v * i,3)
+
+def entrada():
+    letra = input("Digite:\n\"R\" para resistência   "
               "\n\"I\" para corrente  "
               "\n\"V\" para tensão "
               "\n\"P\" para potência  "
               "\n\"S\" para sair - ").upper()
-print()
-    
-while letra != "S":
-    if letra =="I":
-       i = calculo_corrente()
+    return letra
 
-    if letra =="V":
-        v = calculo_tensao()
+def selecao(letra):
+    while letra != "S":
+        if letra =="I":
+           i = calculo_corrente()
+           print("O valor da corrente é", i, "A.")
+           return
 
-    if letra == "R":
-        r = calculo_resistencia()
+        if letra =="V":
+            v = calculo_tensao()
+            print("O valor da tensão é", v, "V")
+            return
 
-    if letra == "P":
-        p = calculo_potencia()
+        if letra == "R":
+            r = calculo_resistencia()
+            print("O valor da resitência é ", r, "ohms")
+            return
 
-    if letra != "S":
-        letra = input("Digite o que quer calcular ou \"S\" para sair: ").upper()
+        if letra == "P":
+            p = calculo_potencia()
+            print("O valor da potência é ", p, "W")
+            return
 
-print("Fim do calculo !")
+    return "S"
+
+def main():
+    letra = entrada()
+    while letra != "S":
+        calculo = selecao(letra)
+        print()
+        letra = entrada()
+    print("Fim do calculo !")
+
+main()
+
